008bfbb5abd79050ad088d9c1e7ebcb01303d454
alexandre <alexandre.leibler@gmail.com>
2016-09-16 10:31:14 -0400

    add basic form to get user input and a route to display the counts

---
+++ app/app.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once(__DIR__ . "/../vendor/autoload.php");
+    require_once(__DIR__ . "/../src/WordRepeatCounter.php");
+
+    $app = new Silex\Application();
+    $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__ . "/../views"));
+
+
+    /*====ROUTES===========================================================================*/
+    $app->get("/", function() use($app) {
+        return $app["twig"]->render("word_count_form.html.twig");
+    });
+
+    $app->get("/term_count", function() use($app) {
+
+        $search_term = $_GET["search_term"];
+        $search_text = $_GET["search_text"];
+
+        $search_counter_instance = new WordRepeatCounter($search_term, $search_text);
+        $count = $search_counter_instance->countRepeats();
+
+        return $app["twig"]->render("search_term_count.html.twig", array("term" => $search_term, "text" => $search_text, "count" => $count));
+    });
+
+    return $app;
+ ?>
